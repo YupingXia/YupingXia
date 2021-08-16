c7a3d4b24e651f3c307b99d926aa3925d4ba8e27
YupingXia <2657625848@qq.com>
2021-08-16 15:00:09 +0800

    Signed-off-by: YupingXia <2657625848@qq.com>

--- C++_Code/shellsort.cpp
+++ C++_Code/shellsort.cpp
@@ -1,40 +1,16 @@
-#include<stdio.h>
-#include<iostream>
-#include<string.h>
-void shellsort(int a[], int len)
+void Shellsort(int a[], int len)  //希尔排序
 {
     for (int x = len / 2; x > 0; x = x / 2)    //计算出每次的比较间隔
     {
-        for (int y = 0; y < x; y++)    //计算出需要进行插入排序的组数
+        for (int y = x; y < len; y++)     //不同增量的插入排序
         {
-            for (int z = y; z < len; z += x)  //进行插入排序操作
+            for (int z = y; (z - x) >= 0 && a[z - x] > a[z]; z -= x)
             {
-                for (int p = z; p > 0; p -= x)
-                {
-                    if (a[p] < a[p - x] && (p - x) >= 0)
-                    {
-                        int tmp = a[p];
-                        a[p] = a[p - x];
-                        a[p - x] = tmp;
-                    }
-                }
+                int tmp = a[z];
+                a[z] = a[z - x];
+                a[z - x] = tmp;
             }
         }
     }
-}
-void showarrary(int a[], int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d,", a[i]);
-    }
-    printf("\n");
-    system("pause");
-}
-int main(void)
-{
-    int a[] = { 4,35,45,234,0,765,23,65,23,43,567,-12,41,67,35,45,51,41,56,-9,-5,0 };
-    int len = sizeof(a) / sizeof(a[0]);    //判断数组长度
-    shellsort(a, len);
-    showarrary(a, len);
+
 }
